487b337bfd6fbf6e89782bdfc054c7c98c2e43c8
Luke Gessler <lukegessler@gmail.com>
2023-08-13 11:13:31 -0600

    don_t include session in load

--- src/main/wat/client/ui/document/document.cljs
+++ src/main/wat/client/ui/document/document.cljs
@@ -17,47 +17,30 @@
   {:query [:project/id :project/name]
    :ident :project/id})
 
-(declare Document)
-(defn do-load!
-  ([app-or-comp doc-id load-opts]
-   (do-load! app-or-comp doc-id load-opts false))
-  ([app-or-comp doc-id load-opts mark-ready?]
-   (let [ident [:document/id doc-id]]
-     (when doc-id
-       (df/load! app-or-comp ident Document
-                 (merge load-opts
-                        (when mark-ready?
-                          {:post-mutation        `dr/target-ready
-                           :post-mutation-params {:target ident}})))))))
-
 (defsc Document
-  [this {:document/keys [id name project] :ui/keys [busy?] :as props}]
-  {:query                [:document/id :document/name
-                          {:document/project (c/get-query ProjectNameQuery)}
-                          :ui/active-tab
-                          :ui/busy?
-                          sn/session-join]
-   :ident                :document/id
-   :pre-merge            (fn [{:keys [data-tree state-map]}]
-                           (merge {:ui/busy? false}
-                                  data-tree))
-   :route-segment        (r/last-route-segment :document)
-   :will-enter           (fn [app {:keys [id] :as route-params}]
-                           (let [parsed-id (gcu/parse-id id)]
-                             (when parsed-id
-                               (dr/route-deferred
-                                 [:document/id parsed-id]
-                                 (fn []
-                                   (do-load! app parsed-id {} true))))))}
+  [this {:document/keys [id name project] :as props}]
+  {:query         [:document/id :document/name
+                   {:document/project (c/get-query ProjectNameQuery)}
+                   sn/session-join]
+   :ident         :document/id
+   :route-segment (r/last-route-segment :document)
+   :will-enter    (fn [app {:keys [id] :as route-params}]
+                    (let [parsed-id (gcu/parse-id id)
+                          ident [:document/id parsed-id]]
+                      (when parsed-id
+                        (dr/route-deferred
+                          ident
+                          (fn []
+                            (df/load! app ident Document
+                                      {:post-mutation        `dr/target-ready
+                                       :post-mutation-params {:target ident}
+                                       :update-query         #(df/elide-query-nodes % #{sn/session-ident})}))))))}
 
   (mui/container {:maxWidth "xl"}
-    (mui/page-title name)
-    (mui/arrow-breadcrumbs {}
-      (mui/link {:color "inherit" :href (r/route-for :projects) :key "projects"} "Projects")
-      (mui/link {:color "inherit" :href (r/route-for :project {:id (:project/id project)}) :key "project"} (:project/name project))
-      (mui/link {:color "textPrimary" :underline "none" :key "document"} name))
-
-    (mui/paper {}
-      (if busy?
-        (wat.client.ui.common.core/loader)
-        "todo"))))
+                 (mui/page-title name)
+                 (mui/arrow-breadcrumbs {}
+                   (mui/link {:color "inherit" :href (r/route-for :projects) :key "projects"} "Projects")
+                   (mui/link {:color "inherit" :href (r/route-for :project {:id (:project/id project)}) :key "project"} (:project/name project))
+                   (mui/link {:color "textPrimary" :underline "none" :key "document"} name))
+                 (mui/paper {}
+                   "todo")))
